81c12eabede7f9aa2ee35c98172a942a83b34974
xiecheng <13136666328@163.com>
2017-08-14 10:18:27 +0800

    jquery 网上商城

---
+++ jquery/23_网上商城/scripts/index.js
@@ -0,0 +1,37 @@
+$(function(){
+    //搜索框
+    $("#inputSearch").on("focus", function(){
+        if(this.value == this.defaultValue){
+            this.value = "";
+        }
+    }).on("blur", function(){
+        if(this.value == ""){
+            this.value = this.defaultValue;
+        }
+    });
+
+    //导航菜单
+    $("#nav li").hover(function(){
+        $(this).children(".jnNav").show();
+    }, function(){
+        $(this).children(".jnNav").hide();
+    });
+
+    //hot
+    $("#jnCatalog .promoted").append("<span class='hot'></span>");
+
+    //轮播图
+    //调整图片的层级关系:z-index
+   /* $("#JS_imgWrap img").each(function(index, elem){
+        $(elem).css({
+            zIndex: 5 - index
+        });
+    });*/
+    $("#jnImageroll div a").on("mouseover", function(){
+        $(this).addClass("chos").siblings().removeClass("chos");
+        $("#JS_imgWrap img").eq($(this).index()).stop().fadeIn().siblings().stop().fadeOut();
+    });
+
+
+
+});
