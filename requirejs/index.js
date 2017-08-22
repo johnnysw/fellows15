2674d6e72ce04d908d62fdbf81c792f542cc691e
xiecheng <13136666328@163.com>
2017-08-22 13:37:56 +0800

    requirejs

--- requirejs/index.js
+++ requirejs/index.js
@@ -1,4 +1,4 @@
-//requirejs
+//requirejs AMD
 define(["sort"], function(sort){
     var arr = [4, 2, 1, 6, 7];
     // var arr = "asdfasf";
@@ -6,7 +6,7 @@ define(["sort"], function(sort){
 });
 
 
-//seajs
+//seajs  CMD
 /*define(function(require){
     var arr = [4, 2, 1, 6, 7];
     var sort = require("sort");
