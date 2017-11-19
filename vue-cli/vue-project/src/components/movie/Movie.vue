a917796addf2a11737db0c81e9fe46fb09092a57
wuying <wuy_123@126.com>
2017-11-19 13:47:08 +0800

    webpack

---
+++ vue-cli/vue-project/src/components/movie/Movie.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class='movie'>
+      <common-header></common-header>
+      <common-footer></common-footer>
+  </div>
+</template>
+<script>
+import CommonHeader from '../common/CommonHeader'
+import CommonFooter from '../common/CommonFooter'
+    export default {
+        data () {
+            return {
+            msg: 'Welcome to Your Vue.js App',
+            aa : "fafdsafds"
+            }
+        },
+        components:{
+            CommonHeader,
+            CommonFooter
+        }
+    }
+</script>
+<style>
+  
+</style>
--- vue-cli/vue-project/src/router/index.js
+++ vue-cli/vue-project/src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import CommonHeader from '@/components/common/CommonHeader'
 import CommonFooter from '@/components/common/CommonFooter'
-
+import Movie from "@/components/movie/Movie"
 Vue.use(Router)
 
 export default new Router({
@@ -15,7 +15,7 @@ export default new Router({
     },
     {
       path     : '/movie',
-      component: CommonFooter
+      component: Movie
     }
   ]
 })
