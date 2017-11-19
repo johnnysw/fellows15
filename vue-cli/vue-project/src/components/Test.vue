d2cc57229a0aa70f8e7d028221ab2b223f4ba3b6
wuying <wuy_123@126.com>
2017-11-19 10:18:31 +0800

    vue-cli

---
+++ vue-cli/vue-project/src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+     this is test
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      aa:"fafdsafds"
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
