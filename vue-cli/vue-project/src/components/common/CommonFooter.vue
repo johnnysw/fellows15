900a0d3c27d3782b5a914e25292d646a6461b859
wuying <wuy_123@126.com>
2017-11-19 11:59:57 +0800

    webpack

---
+++ vue-cli/vue-project/src/components/common/CommonFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class='footer'>
+      <ul>
+          <li>电影</li>
+          <li>音乐</li>
+          <li>书籍</li>
+          <li>图片</li>
+      </ul>
+  </div>
+</template>
+<script>
+    
+</script>
+<style>
+   .footer{
+       height    : 1rem;
+       background: rgb(33, 150, 243);
+   }
+   .footer ul{
+       display: flex;
+   }
+   .footer li{
+       flex       : 1;
+       text-align : center;
+       color      : #fff;
+       line-height: 1rem;
+   }
+</style>
--- vue-cli/vue-project/src/router/index.js
+++ vue-cli/vue-project/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import CommonHeader from '@/components/common/CommonHeader'
+import CommonFooter from '@/components/common/CommonFooter'
+
 Vue.use(Router)
 
 export default new Router({
@@ -13,7 +15,7 @@ export default new Router({
     },
     {
       path     : '/movie',
-      component: CommonHeader
+      component: CommonFooter
     }
   ]
 })
