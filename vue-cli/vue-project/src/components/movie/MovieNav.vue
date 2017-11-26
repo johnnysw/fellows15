b9917fc70a59227985aabae841fe00e8b3fe7d87
wuying <wuy_123@126.com>
2017-11-26 14:01:12 +0800

    webpack

--- vue-cli/vue-project/src/components/movie/MovieNav.vue
+++ vue-cli/vue-project/src/components/movie/MovieNav.vue
@@ -1,30 +1,46 @@
 <template>
-  <div class='nav'>
+  <div class="nav">
       <ul>
-          <li>top250</li>
-          <li>正在热映</li>
-          <li>即将上映</li>
+        <li><router-link to="/movie/top250">top250</router-link></li>
+        <li><router-link to="/movie/hot">正在热映</router-link></li>
+        <li><router-link to="/movie/coming">即将上映</router-link></li>
       </ul>
   </div>
 </template>
+
 <script>
-    
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-   .nav{
-       height    : 1rem;
-       background: rgb(33, 150, 243);
-       position  : fixed;
-       top       : 1rem;
-       width     : 100%;
-   }
-   .nav ul{
-       display: flex;
-   }
-   .nav li{
-       flex       : 1;
-       text-align : center;
-       color      : #fff;
-       line-height: 1rem;
-   }
+      .nav{
+        height    : 1rem;
+        background: rgb(33, 150, 243);
+        position  : fixed;
+        top       : 1rem;
+        width     : 100%;
+      }
+      .nav ul{
+        display: flex;
+      }
+      .nav li{
+        flex       : 1;
+        color      : #fff;
+        line-height: 1rem;
+        text-align : center;
+      }
+      .nav li a{
+        color: #ccc;
+      }
+      .nav li a.router-link-active{
+        color: #fff;
+      }
+  
 </style>
--- vue-cli/vue-project/src/router/index.js
+++ vue-cli/vue-project/src/router/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import CommonHeader from '@/components/common/CommonHeader'
-import CommonFooter from '@/components/common/CommonFooter'
-import Movie from "@/components/movie/Movie"
-import Music from "@/components/music/Music"
-import Book from "@/components/book/Book"
-import Photo from "@/components/photo/Photo"
+import Movie from '@/components/movie/Movie'
+import Music from '@/components/music/Music'
+import Book from '@/components/book/Book'
+import Photo from '@/components/photo/Photo'
+import ComponentA from '@/components/ComponentA'
+import MovieTop250 from '@/components/movie/MovieTop250'
+
+// import CommonFooter from '@/components/common/CommonFooter'
 
 Vue.use(Router)
 
@@ -14,12 +16,16 @@ export default new Router({
   routes: [
     {
       path     : '/',
-      name     : 'HelloWorld',
-      component: HelloWorld
+      component: ComponentA
     },
     {
       path     : '/movie',
-      component: Movie
+      component: Movie,
+      children : [
+        {path:'/movie/top250',component:MovieTop250},
+        {path:'/movie/hot',component:MovieTop250},
+        {path:'/movie/coming',component:MovieTop250}
+      ]
     },
     {
       path     : '/music',
