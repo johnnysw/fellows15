1fad4fe410027f14e0c7eb1366f7b5ab72481a18
wuying <wuy_123@126.com>
2017-12-01 13:40:13 +0800

    webpack

--- vue-cli/vue-project/src/components/common/CommonFooter.vue
+++ vue-cli/vue-project/src/components/common/CommonFooter.vue
@@ -1,36 +1,47 @@
 <template>
-  <div class='footer'>
+  <div class="footer" :style="{background:bgColor}">
       <ul>
-          <li><router-link to='/movie'>电影</router-link></li>
-          <li><router-link to='/music'>音乐</router-link></li>
-          <li><router-link to='/book'>书籍</router-link></li>
-          <li><router-link to='/photo'>图片</router-link></li>
+        <li><router-link to='/movie'>电影</router-link></li>
+        <li><router-link to="/music">音乐</router-link></li>
+        <li><router-link to="/book">书籍</router-link></li>
+        <li><router-link to="/photo">图片</router-link></li>
       </ul>
   </div>
 </template>
+
 <script>
-    
+export default {
+  props: ['bgColor'],
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
 </script>
-<style>
-   .footer{
-       height    : 1rem;
-       background: rgb(33, 150, 243);
-       position  : fixed;
-       bottom    : 0;
-       width     : 100%;
-   }
-   .footer ul{
-       display: flex;
-   }
-   .footer li{
-       flex       : 1;
-       text-align : center;
-       line-height: 1rem;
-   }
-   .footer li a{
-       color: #ccc;
-   }
-   .footer li a.router-link-active{
-       color: #fff;
-   }
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+      .footer{
+        height    : 1rem;
+        background: rgb(33, 150, 243);
+        position  : fixed;
+        bottom    : 0;
+        width     : 100%;
+      }
+      .footer ul{
+        display: flex;
+      }
+      .footer li{
+        flex       : 1;
+        line-height: 1rem;
+        text-align : center;
+      }
+      .footer li a{
+        color: #ccc;
+      }
+      .footer li a.router-link-active{
+        color: #fff;
+      }
+  
 </style>
